c91bcdf24d999c9bca79a15cf615558a3bd14f5f
Jeroen Pelgrims <git@jeroenpelgrims.com>
2024-03-24 14:01:23 +0100

    Ignore TS warning for jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  transform: {
+    "^.+\\.(ts|tsx)?$": [
+      "ts-jest",
+      { diagnostics: { ignoreCodes: ["TS151001"] } },
+    ],
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
 };
