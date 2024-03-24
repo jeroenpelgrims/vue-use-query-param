281bb5c4e662fe52014949ea44f9c91367f8f2cb
Jeroen Pelgrims <git@jeroenpelgrims.com>
2024-03-24 11:42:18 +0100

    Kinda fix typing for the ArrayParam

--- src/converters/ArrayParam.ts
+++ src/converters/ArrayParam.ts
@@ -1,21 +1,28 @@
 import { LocationQueryValue } from "vue-router";
 import { ParamSerializationConfig } from ".";
 
+// Slightly iffy typing with the `as` casts, but yolo.
+
 export function ArrayParam<TParam>(
   elementConfig: ParamSerializationConfig<TParam>
-) {
+): ParamSerializationConfig<NonNullable<TParam>[] | null> {
   return {
-    serialize: (values: TParam[]) => values.map(elementConfig.serialize),
-    deserialize: (values: LocationQueryValue | LocationQueryValue[]) => {
+    serialize: (values) => {
+      if (!values) {
+        return null;
+      }
+      return values.map(elementConfig.serialize) as LocationQueryValue[];
+    },
+    deserialize: (values) => {
       if (!values) {
         return null;
       }
 
       if (!Array.isArray(values)) {
-        return [values];
+        return [elementConfig.deserialize(values)] as NonNullable<TParam>[];
       }
 
-      return values.map(elementConfig.deserialize);
+      return values.map(elementConfig.deserialize) as NonNullable<TParam>[];
     },
-  } as ParamSerializationConfig<Array<NonNullable<TParam>> | null>;
+  };
 }
