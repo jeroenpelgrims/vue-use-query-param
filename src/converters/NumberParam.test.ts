c34ceb36d221ee44798e1d8bb778805aa37f3499
Jeroen Pelgrims <git@jeroenpelgrims.com>
2024-03-24 14:09:13 +0100

    Add tests for NumberParam

---
+++ src/converters/NumberParam.test.ts
@@ -0,0 +1,40 @@
+import { NumberParam } from "./NumberParam";
+
+describe("DateParam", () => {
+  const { serialize, deserialize } = NumberParam;
+
+  describe("serialize", () => {
+    test("null", () => {
+      expect(serialize(null)).toBe(null);
+    });
+    test("undefined", () => {
+      expect(serialize(undefined as unknown as number)).toBe(null);
+    });
+    test("integer", () => {
+      expect(serialize(123)).toBe("123");
+    });
+    test("float", () => {
+      expect(serialize(3.1415)).toBe("3.1415");
+    });
+    test("string", () => {
+      expect(serialize("3.1415" as unknown as number)).toBe(null);
+    });
+  });
+  describe("deserialize", () => {
+    test("null", () => {
+      expect(deserialize(null)).toBe(null);
+    });
+    test("undefined", () => {
+      expect(deserialize(undefined as unknown as string)).toBe(null);
+    });
+    test("integer", () => {
+      expect(deserialize("123")).toBe(123);
+    });
+    test("float", () => {
+      expect(deserialize("3.1415")).toBe(3.1415);
+    });
+    test("String value", () => {
+      expect(deserialize("invalid")).toBe(null);
+    });
+  });
+});
